68aa013f12bcfb68276a2b83760b26c6de2e378a
Tanmoy-Santra <tanmoysantra911@gmail.com>
2025-06-18 11:47:07 +0530

    commit

--- ai-ticket-frontend/src/main.jsx
+++ ai-ticket-frontend/src/main.jsx
@@ -1,62 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+// main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 import "./index.css";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import CheckAuth from "./components/CheckAuth.jsx";
-import Tickets from "./pages/tickets.jsx";
-import TicketDetailsPage from "./pages/TicketDetailsPage.jsx";
-import Login from "./pages/login.jsx";
-import Signup from "./pages/signup.jsx";
-import Admin from "./pages/admin.jsx";
-import Navbar from "./components/Navbar.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <CheckAuth protectedRoute={true}>
-              <Tickets />
-            </CheckAuth>
-          }
-        />
-        <Route
-          path="/tickets/:id"
-          element={
-            <CheckAuth protectedRoute={true}>
-              <TicketDetailsPage />
-            </CheckAuth>
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <CheckAuth protectedRoute={false}>
-              <Login />
-            </CheckAuth>
-          }
-        />
-        <Route
-          path="/signup"
-          element={
-            <CheckAuth protectedRoute={false}>
-              <Signup />
-            </CheckAuth>
-          }
-        />
-        <Route
-          path="/admin"
-          element={
-            <CheckAuth protectedRoute={true}>
-              <Admin />
-            </CheckAuth>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
